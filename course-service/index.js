a83bef99979768f2699badb5c8a83883e8951017
annd2 <an.ngdinh@gmail.com>
2022-05-12 16:22:57 +0700

    Init npm project

---
+++ course-service/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const PORT = 8080;
+const HOST = "0.0.0.0";
+
+// App
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World - an!\n');
+});
+
+app.listen(PORT, HOST);
+console.log('Running on http://${HOST}:${PORT}');
